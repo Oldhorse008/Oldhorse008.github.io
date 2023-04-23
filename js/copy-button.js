3bcfa87c1fa102a26cffe6262c523ddc2a83daff
GCDM-LIFE <2013595524@qq.com>
2023-04-23 19:51:55 +0800

    main

--- js/copy-button.js
+++ js/copy-button.js
@@ -1,23 +1,16 @@
-var codeBlocks = document.querySelectorAll("pre code");
-
-codeBlocks.forEach(function (codeBlock) {
-  var copyButton = document.createElement('button');
-  copyButton.className = 'copy';
-  copyButton.type = 'button';
-  copyButton.ariaLabel = 'Copy code to clipboard';
-  copyButton.innerText = 'Copy';
-
-  codeBlock.append(copyButton);
-
-  copyButton.addEventListener('click', function () {
-    var code = codeBlock.querySelector('code').innerText.trim();
-    window.navigator.clipboard.writeText(code);
-
-    copyButton.innerText = 'Copied';
-    var fourSeconds = 4000;
-
-    setTimeout(function () {
-      copyButton.innerText = 'Copy';
-    }, fourSeconds);
-  });
-});
+hljs.addPlugin(
+  new CopyButtonPlugin({
+    hook: (text, el) => {
+      let { replace, replacewith } = el.dataset;
+      if (replace && replacewith) {
+        return text.replace(replace, replacewith);
+      }
+      return text;
+    },
+    callback: (text, el) => {
+      /* logs `gretel configure --key grtf32a35bbc...` */
+      console.log(text);
+    },
+  })
+);
+hljs.highlightAll();
